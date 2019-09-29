8f546833e5d3c921b87c71593023bd8d0af99806
DaveVodrazka <dave.vodrazka@gmail.com>
2019-09-29 14:23:10 +0200

    fix: utm no longer breaks pagination

--- src/libs/utm/persistentUtm.js
+++ src/libs/utm/persistentUtm.js
@@ -7,16 +7,39 @@ const getUtm = () => {
 	return utm;
 };
 
+const utmToArr = (utm) => {
+	const utmArr = utm.split('&');
+	return utmArr;
+};
+
 const utmToLinks = () => {
 	const utm = getUtm();
 	if (!utm) {
 		return null;
 	}
+	const arrOfUtms = utmToArr(utm);
+	let utmToAppend = '';
+	arrOfUtms.forEach((attribute, index, array) => {
+		if (attribute.includes('utm_source=') || attribute.includes('utm_medium=')) {
+			utmToAppend = utmToAppend.concat(attribute);
+			if (index != (array.length - 1)) {
+				utmToAppend = utmToAppend.concat('&');
+			}
+		}
+	});
+	if (!utmToAppend) {
+		return null;
+	}
+
 	const links = document.querySelectorAll('a');
 	links.forEach((link) => {
 		const currentUrl = link.href;
 		if (currentUrl.includes('www.fondik.cz')) {
-			link.href = currentUrl + '?' + utm;
+			if (currentUrl.includes('?')) {
+				link.href = currentUrl + '&' + utmToAppend;
+			} else {
+				link.href = currentUrl + '?' + utmToAppend;
+			}	
 		}
 	});
 };
