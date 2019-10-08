43a3b687d96e30f7aac8be91e2f9e6f9d6fa6b36
DaveVodrazka <dave.vodrazka@gmail.com>
2019-10-08 22:44:06 +0200

    feat: based on UTM call either seznam or adsense

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import stickySky from './libs/stickySky/stickySky.js';
 import callAds from './libs/ads/callAds.js';
-import utmToLinks from './libs/utm/persistentUtm.js';
+import { utmToLinks, getUtm } from './libs/utm/persistentUtm.js';
 
-stickySky();
-callAds();
 utmToLinks();
+const utm = getUtm();
+if (utm.includes('utm_source=www.seznam.cz') && utm.includes('utm_medium=sekce-z-internetu')) {
+	console.log('calling Seznam ads');
+	stickySky();
+	callAds();
+} else {
+	console.log('calling Google ads');
+	(adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-1062420095711039",
+    enable_page_level_ads: true
+  });
+}
