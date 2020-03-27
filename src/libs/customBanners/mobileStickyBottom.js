3710cdbe8aa4844ff940f84af5ed22f38a08a2c4
DaveVodrazka <dave.vodrazka@gmail.com>
2020-03-27 20:17:27 +0100

    feat: add mobile banner appear on scroll

--- src/libs/customBanners/mobileStickyBottom.js
+++ src/libs/customBanners/mobileStickyBottom.js
@@ -3,7 +3,22 @@ const detectMobileBanner = () => {
   const banner = document.getElementById('mobile-fondik-banner');
   if (!banner) return false;
   if (banner.classList && banner.classList.contains('w-condition-invisible')) return false;
+  renderMobileBanner(banner);
   return true;
+};
+
+const renderMobileBanner = (banner) => {
+  banner.style.marginBottom = - banner.offsetHeight + 'px';
+  banner.style.opacity = 1;
+  const scrollBreaker = document.querySelector('.in-article-detail-holder');
+  if (!scrollBreaker) return;
+  window.addEventListener('scroll', () => {
+    const scroll = scrollBreaker.getBoundingClientRect().bottom;
+    if (scroll <= 0) {
+      banner.style.transition = 'all ease-out .7s';
+      banner.style.marginBottom = '0px';
+    }
+  });
 };
 
 export default detectMobileBanner;
