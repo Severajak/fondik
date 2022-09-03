d92aa0ad4d80f446adeda79d5e4086b466c4538e
Vodrazka, David <david.vodrazka@firma.seznam.cz>
2022-09-03 17:04:31 +0200

    fix: do not display topSponzor if top is 0

--- src/libs/stickySky/createSky.js
+++ src/libs/stickySky/createSky.js
@@ -7,7 +7,7 @@ import { debug } from '../debug.js';
 const createSky = ({ side = 'right', top = 0 }) => {
 	const holder = document.createElement('div');
 	const sky = document.createElement('div');
-	holder.appendChild(sky);
+	const topSponzor = document.getElementById('ssp-zone-118564');
 
 	// holder style
 	holder.style.position = 'absolute';
@@ -25,6 +25,14 @@ const createSky = ({ side = 'right', top = 0 }) => {
 	sky.style.width = '300px';
 	sky.style.height = '600px';
 	sky.id = 'ssp-zone-118569';
+
+	holder.appendChild(sky);
+
+	if (topSponzor && top === 0) {
+		// do not display topSponzor
+		topSponzor.style.display = 'none';
+	}
+
 	debug('Holder and sky created:');
 	debug({ holder, sky });
 	return { holder, sky };
